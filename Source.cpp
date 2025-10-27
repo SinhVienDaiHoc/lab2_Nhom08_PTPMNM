3b5d3d0e4332796165aac8837e085981d4ed33e5
Nghia <nghiango2004@gmail.com>
2025-10-27 10:48:28 +0700

    Nghia_tong

--- Source.cpp
+++ Source.cpp
@@ -5,10 +5,18 @@ int tong(int a, int b);
 int hieu(int a, int b);
 int tich(int a, int b);
 float thuong(int a, int b);
-int main()
-{
-	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
 
+int tong(int a, int b){
+return a+b;
+};
+int main()
+{	int a, b;
+	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT" << endl;
+	cout << "Nhap a:";
+	cin >> a;
+	cout << "\nNhap b:";
+	cin >> b;
+	cout << "Gia tri trong la:" << tong(a,b)<<endl;
 	system("pause");
 	return 0;
 }
